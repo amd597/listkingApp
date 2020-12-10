b49c776bef45cdbd16fdf7c12230b98504e8dbe5
Ahmed <amd597@icloud.com>
2020-12-10 12:25:54 -0500

    added .gitignore

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+require('dotenv').config();
 const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
+const MDB_PW = process.env.MDB_PW;
 var PORT = process.env.PORT || 3000;
+
 
 app.use(bodyParser.urlencoded({ extended: true}))
 app.use(express.static("public"))
@@ -9,7 +12,7 @@ app.set("view engine", "ejs")
 app.use(bodyParser.json())
 
 const MongoClient = require('mongodb').MongoClient
-MongoClient.connect('mongodb://amd597:K6T5rswZJtpWWaTJ@cluster0-shard-00-00.ktqxt.mongodb.net:27017,cluster0-shard-00-01.ktqxt.mongodb.net:27017,cluster0-shard-00-02.ktqxt.mongodb.net:27017/listking?ssl=true&replicaSet=atlas-10myl8-shard-0&authSource=admin&retryWrites=true&w=majority', { useUnifiedTopology: true })
+MongoClient.connect(`mongodb://amd597:${MDB_PW}@cluster0-shard-00-00.ktqxt.mongodb.net:27017,cluster0-shard-00-01.ktqxt.mongodb.net:27017,cluster0-shard-00-02.ktqxt.mongodb.net:27017/listking?ssl=true&replicaSet=atlas-10myl8-shard-0&authSource=admin&retryWrites=true&w=majority`, { useUnifiedTopology: true })
     .then(client => {
        const db = client.db("listking")
        const listCollection = db.collection("lists")
@@ -53,3 +56,4 @@ MongoClient.connect('mongodb://amd597:K6T5rswZJtpWWaTJ@cluster0-shard-00-00.ktqx
 
 app.listen(PORT)
     
+
